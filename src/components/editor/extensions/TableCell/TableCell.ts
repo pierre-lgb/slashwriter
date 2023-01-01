63c9ac021aa406eace994b1fa0102c8389be61cc
pierre-lgb <plegoubin.acc@gmail.com>
2023-01-01 23:11:40 +0100

    Set table cell default background to "none" instead of null

--- src/components/editor/extensions/TableCell/TableCell.ts
+++ src/components/editor/extensions/TableCell/TableCell.ts
@@ -33,7 +33,7 @@ export default Node.create<TableCellOptions>({
                 }
             },
             background: {
-                default: null
+                default: "none"
             }
         }
     },
