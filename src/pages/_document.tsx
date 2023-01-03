ea71f38602812b4998d6a74eb75f870eb71a24ca
pierre-lgb <plegoubin.acc@gmail.com>
2023-01-03 20:36:00 +0100

    Custom _document.js for CSS SSR

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,7 @@
-import { Head, Html, Main, NextScript } from "next/document"
+import Document, { DocumentContext, Head, Html, Main, NextScript } from "next/document"
+import { ServerStyleSheet } from "styled-components"
 
-export default function Document() {
+export default function CustomDocument() {
     return (
         <Html lang="fr">
             <Head>
@@ -21,4 +22,25 @@ export default function Document() {
             </body>
         </Html>
     )
+}
+
+CustomDocument.getInitialProps = async (ctx: DocumentContext) => {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+        ctx.renderPage = () =>
+            originalRenderPage({
+                enhanceApp: (App) => (props) =>
+                    sheet.collectStyles(<App {...props} />)
+            })
+
+        const initialProps = await Document.getInitialProps(ctx)
+        return {
+            ...initialProps,
+            styles: [initialProps.styles, sheet.getStyleElement()]
+        }
+    } finally {
+        sheet.seal()
+    }
 }
